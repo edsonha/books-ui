d679ab9d9c32ffe855ba96414cdb60166c1fb7d6
edsonha <edsonha@gmail.com>
2020-03-25 23:16:52 +0800

    2. Display book data into responsive table using axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return <h1>Hello World from Library Book App!</h1>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookData: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:3001/books").then(
+      res => {
+        this.setState({
+          bookData: res.data
+        });
+      },
+      err => {
+        console.log(err.message);
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="center">Hello World from Library Book App!</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Book Title</th>
+              <th>Author</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.bookData.map(book => {
+              return (
+                <tr key={book._id}>
+                  <td>{book._id}</td>
+                  <td>{book.title}</td>
+                  <td>{book.author}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
